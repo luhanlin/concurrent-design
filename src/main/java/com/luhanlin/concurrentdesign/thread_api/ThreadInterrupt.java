3c121700cb1a334e21c49efdcf2a481af2685b57
UC253590 <HANLIN.LU@thomsonreuters.com>
2019-02-12 19:24:56 +0800

    Mr.lu 多线程知识巩固join

---
+++ src/main/java/com/luhanlin/concurrentdesign/thread_api/ThreadInterrupt.java
@@ -0,0 +1,24 @@
+package com.luhanlin.concurrentdesign.thread_api;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadInterrupt {
+
+    public static void main(String[] args) {
+
+        // 判断当前线程是否中断 使用interrupted（）
+        System.out.println("Main is interrupted? " + Thread.interrupted());
+
+        // 中断当前线程
+        Thread.currentThread().interrupt();
+
+        // 判断当前线程是否中断 isInterrupted（）
+        System.out.println("Main is interrupted? " + Thread.currentThread().isInterrupted());
+
+        try {
+            TimeUnit.MINUTES.sleep(1L);
+        } catch (InterruptedException e) {
+            System.out.println("I will be interrupted still.");
+        }
+    }
+}
---
+++ src/main/java/com/luhanlin/concurrentdesign/thread_api/fiight/FightQuery.java
@@ -0,0 +1,8 @@
+package com.luhanlin.concurrentdesign.thread_api.fiight;
+
+import java.util.List;
+
+public interface FightQuery {
+
+    List<String> get();
+}
